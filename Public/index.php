0f307c64a9dc1fa8593d9d67396ac2dcb1b4fef8
Capitan Cloud <my25mb@has.im>
2024-10-12 00:47:17 +0200

    Add files via upload

--- Public/index.php
+++ Public/index.php
@@ -926,20 +926,20 @@ upload();
 <!--
     Copyright 2021, 2024 5 Mode
 
-    This file is part of Homolog.
+    This file is part of DropIn.
 
-    Homolog is free software: you can redistribute it and/or modify
+    DropIn is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Homolog is distributed in the hope that it will be useful,
+    DropIn is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Homologs. If not, see <https://www.gnu.org/licenses/>.
+    along with DropIn. If not, see <https://www.gnu.org/licenses/>.
  -->
   
     
